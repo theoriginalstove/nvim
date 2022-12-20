1576726fa6f09ffa4e70ade9f43ce1c224b986ae
Steven Turturo <steven.turturo@tsi.com>
2022-12-20 14:14:31 -0600

    feat: add everforest theme

---
+++ lua/catppuccin/palettes/everforest.lua
@@ -0,0 +1,32 @@
+local color_palette = {
+	rosewater = "#4C3743",
+	flamingo = "#D699B6",
+	pink = "#D699B6",
+	mauve = "#59464C",
+	red = "#E67E80",
+	maroon = "#E67E80",
+	peach = "#E69875",
+	yellow = "#DBBC7F",
+	green = "#A7C080",
+	teal = "#7FBBB3",
+	sky = "#555F66",
+	sapphire = "#4D5960",
+	blue = "#3F5865",
+	lavender = "#4C3743",
+
+	text = "#D3C6AA",
+	subtext1 = "#9DA9A0",
+	subtext0 = "#859289",
+	overlay2 = "#7A8478",
+	overlay1 = "#4F5B58",
+	overlay0 = "#495156",
+	surface2 = "#414B50",
+	surface1 = "#374145",
+	surface0 = "#2E383C",
+
+	base = "#293136",
+	mantle = "#333C43",
+	crust = "#3A464C",
+}
+
+return color_palette
